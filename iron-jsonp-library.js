464341ace022bba2d5b533bd6ed6b33889db3a40
Elliott Marquez <emarquez@google.com>
2018-07-16 17:57:08 -0700

    cleanup element

--- iron-jsonp-library.js
+++ iron-jsonp-library.js
@@ -1,18 +1,18 @@
 /**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+This code may only be used under the BSD style license found at
+http://polymer.github.io/LICENSE.txt The complete set of authors may be found at
+http://polymer.github.io/AUTHORS.txt The complete set of contributors may be
+found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by Google as
+part of the polymer project is also subject to an additional IP rights grant
+found at http://polymer.github.io/PATENTS.txt
 */
-import { Base } from '@polymer/polymer/polymer-legacy.js';
-
-import { Polymer as Polymer$0 } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import {Polymer} from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import {Base} from '@polymer/polymer/polymer-legacy.js';
 
 /**
- * `Polymer.IronJsonpLibraryBehavior` loads a jsonp library.
+ * `IronJsonpLibraryBehavior` loads a jsonp library.
  * Multiple components can request same library, only one copy will load.
  *
  * Some libraries require a specific global function be defined.
@@ -31,12 +31,21 @@ export const IronJsonpLibraryBehavior = {
     /**
      * True if library has been successfully loaded
      */
-    libraryLoaded: {type: Boolean, value: false, notify: true, readOnly: true},
+    libraryLoaded: {
+      type: Boolean,
+      value: false,
+      notify: true,
+      readOnly: true,
+    },
     /**
      * Not null if library has failed to load
      */
-    libraryErrorMessage:
-        {type: String, value: null, notify: true, readOnly: true}
+    libraryErrorMessage: {
+      type: String,
+      value: null,
+      notify: true,
+      readOnly: true,
+    }
     // Following properties are to be set by behavior users
     /**
      * Library url. Must contain string `%%callback%%`.
@@ -214,14 +223,9 @@ Loader.prototype = {
   Implemented by  Polymer.IronJsonpLibraryBehavior. Use it
   to create specific library loader elements.
 
-  @demo
+  @demo demo/index.html
 */
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
-Polymer$0({
+Polymer({
 
   is: 'iron-jsonp-library',
 
